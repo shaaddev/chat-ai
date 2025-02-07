4fd68da7f24952852ae441e41338f18f7cb279bf
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-02-07 02:36:41 -0500

    models-popover (refactor)

--- components/models-popover.tsx
+++ components/models-popover.tsx
@@ -6,10 +6,14 @@ import {
   LucideIcon,
 } from "lucide-react";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ImageTooltip, UnstableTooltip } from "./model-helpful-tooltips";
 
 interface model_selection {
@@ -59,54 +63,69 @@ export function ModelsPopover() {
   ];
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="outline-none focus:outline-none">
         <button
           type="button"
           className="inline-flex items-center gap-1 px-3 py-1 text-sm text-neutral-300 hover:text-neutral-100"
         >
           Gemini 1.5 pro
-          <ChevronDown className="h-4 w-4" />
+          <ChevronDown className="size-4" />
         </button>
-      </PopoverTrigger>
-      <PopoverContent className="w-96 bg-neutral-950 rounded-xl border-neutral-800">
-        <div className="px-5 py-1 text-sm font-medium text-muted-foreground">
-          Standard Models
-        </div>
-        {stable_models.map((m, index) => (
-          <div
-            key={index}
-            className="w-full flex items-center justify-between hover:bg-neutral-800 hover:cursor-pointer rounded-xl px-5 py-2"
-          >
-            <li className="flex items-center gap-2">
-              <span className="font-medium">{m.model}</span>
-              <m.icon className="size-4" />
-            </li>
-            <div>
-              <ImageTooltip>
-                <m.image className="size-4 text-blue-600" />
-              </ImageTooltip>
-            </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[--radix-dropdown-menu-trigger-width] min-w-96 rounded-xl">
+        <DropdownMenuLabel className=" font-normal">
+          <div className="flex-1 text-left leading-tight">
+            <span>Standard Models</span>
           </div>
-        ))}
-        <div className="w-full border-b border-neutral-700 my-5"></div>
-        {experimental_models.map((m, index) => (
-          <div
-            key={index}
-            className="w-full flex items-center justify-between hover:bg-neutral-800 hover:cursor-pointer rounded-xl px-5 py-2"
-          >
-            <li className="flex items-center gap-2">
-              <span className="font-medium">{m.model}</span>
-              <m.icon className="size-4" />
-            </li>
-            <div>
-              <UnstableTooltip>
-                <m.unstable className="size-4 text-orange-600" />
-              </UnstableTooltip>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          {stable_models.map((m, index) => (
+            <DropdownMenuItem
+              key={index}
+              className="rounded-xl justify-between flex py-4"
+            >
+              <div className="flex items-center gap-2">
+                <span className="font-medium">{m.model}</span>
+                <m.icon className="size-4" />{" "}
+                {/* add tooltip component here later */}
+              </div>
+              <div>
+                <ImageTooltip>
+                  <m.image className="size-4 text-blue-600" />
+                </ImageTooltip>
+              </div>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex-1 text-left leading-tight">
+              <span>Experimental Models</span>
             </div>
-          </div>
-        ))}
-      </PopoverContent>
-    </Popover>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            {experimental_models.map((m, index) => (
+              <DropdownMenuItem
+                key={index}
+                className="rounded-xl justify-between flex py-4"
+              >
+                <div className="flex items-center gap-2">
+                  <span className="font-medium'">{m.model}</span>
+                  <m.icon className="size-4" />{" "}
+                  {/* add tooltip component here later */}
+                </div>
+                <div>
+                  <UnstableTooltip>
+                    <m.unstable className="size-4 text-orange-600" />
+                  </UnstableTooltip>
+                </div>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
