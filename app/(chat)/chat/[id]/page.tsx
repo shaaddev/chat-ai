f0845fec6dd90810aadab1f4853895550c41a061
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-02-11 22:56:30 -0500

    clean-up

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -2,8 +2,6 @@ import { Chat } from "@/components/chat";
 import { notFound } from "next/navigation";
 import { getChatById, getMessagesByChatId } from "@/db/queries";
 import { convertToUIMessages } from "@/lib/utils";
-import { cookies } from "next/headers";
-// import { DEFAULT_CHAT_MODEL } from "@/lib/ai/models";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -18,26 +16,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     id,
   });
 
-  const cookie = await cookies();
-  const chatModelFromCookie = cookie.get("selected_model");
-
-  if (!chatModelFromCookie) {
-    return (
-      <>
-        <Chat
-          id={chat.id}
-          // selectedChatModel={DEFAULT_CHAT_MODEL}
-          initialMessages={convertToUIMessages(messagesFromDb)}
-        />
-      </>
-    );
-  }
-
   return (
     <>
       <Chat
         id={chat.id}
-        // selectedChatModel={chatModelFromCookie.value}
         initialMessages={convertToUIMessages(messagesFromDb)}
       />
     </>
--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,25 +1,8 @@
 import { Chat } from "@/components/chat";
 import { generateUUID } from "@/lib/utils";
-// import { DEFAULT_CHAT_MODEL } from "@/lib/ai/models";
-import { cookies } from "next/headers";
 
 export default async function Page() {
   const id = generateUUID();
-
-  const cookieStore = await cookies();
-  const modelFromCookie = cookieStore.get("selected_model");
-
-  if (!modelFromCookie) {
-    return (
-      <div className="flex flex-col min-h-screen w-full">
-        <Chat
-          id={id}
-          // selectedChatModel={DEFAULT_CHAT_MODEL}
-          initialMessages={[]}
-        />
-      </div>
-    );
-  }
 
   return (
     <div className="flex flex-col min-h-screen w-full">
