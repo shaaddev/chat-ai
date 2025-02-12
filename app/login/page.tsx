0697c46f8c2b86305562c21cc5d18f9821b9fcfd
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-02-12 02:37:30 -0500

    added otp form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,48 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { Github, Home } from "lucide-react";
 import { Link } from "next-view-transitions";
-import type { JSX } from "react";
-
-interface btnProps {
-  className: string;
-  size: "default" | "sm" | "lg" | "icon" | null | undefined;
-  icon: JSX.Element;
-  text: string;
-}
+import { Button } from "@/components/ui/button";
+import { MoveLeft } from "lucide-react";
+import { OTPForm } from "@/components/otp-form";
 
 export default function Page() {
-  const btn: btnProps[] = [
-    {
-      className:
-        "bg-neutral-800 rounded-xl text-neutral-200 hover:bg-neutral-900/50 border border-neutral-50/20 text-lg",
-      size: "lg",
-      icon: <Github className="size-5" />,
-      text: "Continue with Github",
-    },
-    {
-      className:
-        "bg-neutral-800 rounded-xl text-neutral-200 hover:bg-neutral-900/50 border border-neutral-50/20 text-lg",
-      size: "sm",
-      icon: <Home className="size-5 inline" />,
-      text: "Back to Home",
-    },
-  ];
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-8 bg-neutral-900">
-      {btn.map((b, index) => (
-        <Button key={index} className={`${b.className}`} size={b.size}>
-          {b.size == "sm" ? (
-            <Link href="/" className="flex flex-row items-center gap-2">
-              {b.icon} {b.text}
-            </Link>
-          ) : (
-            <>
-              {b.icon} {b.text}
-            </>
-          )}
-        </Button>
-      ))}
+      <OTPForm />
+      <Button type="button" variant={"link"}>
+        <Link
+          href="/"
+          className="text-blue-400 flex flex-row items-center gap-2"
+        >
+          <MoveLeft className="size-4 inline" />
+          Back
+        </Link>
+      </Button>
     </div>
   );
 }
---
+++ components/otp-form.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { toast } from "sonner";
+
+const Schema = z.object({
+  pin: z.string().min(6, {
+    message: "Your one-time password must be 6 characters",
+  }),
+});
+
+export function OTPForm() {
+  const form = useForm<z.infer<typeof Schema>>({
+    resolver: zodResolver(Schema),
+    defaultValues: {
+      pin: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof Schema>) {
+    toast.success(
+      `Congrats! \n Here is the data: \n${(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      )}`
+    );
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-center  text-center gap-8"
+      >
+        <FormField
+          control={form.control}
+          name="pin"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl font-semibold">
+                Verification
+              </FormLabel>
+              <FormDescription>
+                We have sent a code to m@example.com. Enter it below
+              </FormDescription>
+              <FormControl>
+                <InputOTP maxLength={6} {...field}>
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
