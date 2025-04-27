f6cc13491e15202596ef63c814f907932dd18656
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-04-27 01:07:32 -0400

    refactor: prettier

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -62,7 +62,7 @@ export async function POST(req: Request) {
   });
 
   const selectedModel = stable_models.find(
-    (model) => model.id === selectedChatModel
+    (model) => model.id === selectedChatModel,
   );
 
   if (!selectedModel) {
@@ -82,7 +82,7 @@ export async function POST(req: Request) {
           try {
             const assistantId = getTrailingMessageId({
               messages: response.messages.filter(
-                (message) => message.role === "assistant"
+                (message) => message.role === "assistant",
               ),
             });
 
